91039d2b2914206af550b2714ea681ff740ec159
tiko <tiko.pb@gmail.com>
2020-12-29 09:36:56 +0700

    1.7 parsing variable from url

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -28,24 +28,31 @@ const dataObj = JSON.parse(data);
 ////////////////////
 // SERVER
 const server = http.createServer((req, res)=>{
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     // overview page 
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {'Content-type': 'text/html'});
 
         const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
         res.end(output);
 
-    }else if (pathName === '/api'){
+    }else if (pathname === '/api'){
         res.writeHead(200, {'Content-type': 'application/json' });
         res.end(data);
     }
-    else if(pathName === '/product'){
+
+    //product page 
+    else if(pathname === '/product'){
+        //res.writeHead(200, {'Content-type': 'application/json' });
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(tempProduct);
-    }else {
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+    }
+    //page not found 
+    else {
         res.writeHead(404);
         res.end('Page Not Found!');
     }
