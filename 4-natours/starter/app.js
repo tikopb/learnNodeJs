6de4b353bf05e90c9e399b2ed899eafe5b19ae8f
tiko <tiko.pb@gmail.com>
2021-03-04 19:39:31 +0700

    4.2 Starting make API
    4.2 Starting make API

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
+/*
 app.get('/', (req, res) => {
     res.status(200)
     .json({message: 'Hello from the server side', app: 'natours'});
@@ -10,8 +12,23 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     res.send('You can post from here ...');
 })
+*/
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(' App running on port ${port}...');
+    console.log(`App running on port ${port}...`);
 });
