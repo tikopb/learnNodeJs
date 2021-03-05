37278824a7067433d905ba26059226cd6e336902
tiko <tiko.pb@gmail.com>
2021-03-05 19:37:12 +0700

    4.4

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -30,6 +30,24 @@ app.get('/api/v1/tours', (req,res) => {
     });
 });
 
+app.get('/api/v1/tours/:id', (req,res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    
+    if(id > tours.length) {
+        return ""
+    }
+    
+    const tour = tours.find(el => el.id === id)
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+});
+
 app.post('/api/v1/tours', (req, res) => {
     //console.log(req.body);
 
