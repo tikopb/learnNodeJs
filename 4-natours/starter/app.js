2e62dc6fe0b1b373d305c0f5b76bd11d5d115ba7
tiko <tiko.pb@gmail.com>
2021-03-05 15:45:28 +0700

    4.4 Responding URL Parameter
    4.4 Responding URL Parameter

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -30,6 +30,27 @@ app.get('/api/v1/tours', (req,res) => {
     });
 });
 
+app.get('/api/v1/tours/:id', (req,res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+ 
+    if(id > tours.length){
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'invalid ID'
+        });
+    }
+
+    const tour = tours.find(el => el.id === id);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    });
+});
+
+
 app.post('/api/v1/tours', (req, res) => {
     //console.log(req.body);
 
